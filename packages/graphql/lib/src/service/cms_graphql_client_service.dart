40f91ddeeaabcedbba3e42c3e98f3ae8053ff73d
jakub <jakub.walusiak@utopiasoft.io>
2026-07-01 13:20:35 +0200

    refactor(utopia_cms_graphql): migrate to utopia_arch and drop http/reporter deps
    Replace utopia_utils with utopia_arch and remove the unused http and
    utopia_reporter dependencies; reformat under utopia_lints.

--- packages/graphql/lib/src/service/cms_graphql_client_service.dart
+++ packages/graphql/lib/src/service/cms_graphql_client_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:gql/language.dart';
 import 'package:graphql/client.dart';
-import 'package:utopia_reporter/utopia_reporter.dart';
+import 'package:utopia_arch/utopia_arch.dart';
 
 class CmsGraphQLClientService {
   const CmsGraphQLClientService();
@@ -14,7 +14,7 @@ class CmsGraphQLClientService {
     Reporter? reporter,
   }) {
     final link = Link.from([
-      if(reporter != null) _ReporterLink(reporter),
+      if (reporter != null) _ReporterLink(reporter),
       if (tokenProvider != null) AuthLink(headerKey: header, getToken: tokenProvider),
       HttpLink(uri),
     ]);
@@ -35,18 +35,22 @@ class _ReporterLink extends Link {
 
   @override
   Stream<Response> request(Request request, [NextLink? forward]) async* {
-    reporter.info([
-      "GraphQL request:",
-      printNode(request.operation.document),
-      if (request.variables.isNotEmpty) "Variables: ${_json.convert(request.variables)}",
-    ].join("\n"));
+    reporter.info(
+      [
+        "GraphQL request:",
+        printNode(request.operation.document),
+        if (request.variables.isNotEmpty) "Variables: ${_json.convert(request.variables)}",
+      ].join("\n"),
+    );
     await for (final response in forward!(request)) {
       final method = response.errors != null ? reporter.warning : reporter.info;
-      method([
-        "GraphQL response:",
-        if (response.data != null) _json.convert(response.data!),
-        if (response.errors != null) "Errors: ${response.errors!.map((it) => it.toString()).join("\n")}",
-      ].join("\n"));
+      method(
+        [
+          "GraphQL response:",
+          if (response.data != null) _json.convert(response.data!),
+          if (response.errors != null) "Errors: ${response.errors!.map((it) => it.toString()).join("\n")}",
+        ].join("\n"),
+      );
       yield response;
     }
   }
